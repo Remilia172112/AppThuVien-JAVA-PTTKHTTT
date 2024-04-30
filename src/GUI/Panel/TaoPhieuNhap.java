14be810564e8f4623ec030a67a2bff300f25c051
CriinWa <nguyenkhoi.watanabe@gmail.com>
2024-04-30 22:34:13 +0700

    taophieunhap(Giaodien) , chucnang chu7a xong

--- src/GUI/Panel/TaoPhieuNhap.java
+++ src/GUI/Panel/TaoPhieuNhap.java
@@ -2,27 +2,27 @@ package GUI.Panel;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
+import javax.swing.text.PlainDocument;
 
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 
 import BUS.NhaCungCapBUS;
 import BUS.PhieuNhapBUS;
@@ -33,6 +33,7 @@ import DTO.SanPhamDTO;
 import GUI.Main;
 import GUI.Component.ButtonCustom;
 import GUI.Component.InputForm;
+import GUI.Component.NumericDocumentFilter;
 import GUI.Component.PanelBorderRadius;
 import GUI.Component.SelectForm;
 
@@ -42,9 +43,9 @@ public final class TaoPhieuNhap extends JPanel implements ItemListener, ActionLi
     PanelBorderRadius left, right;
     JTable tablePhieuNhap, tableSanPham; //tablePhieuNhap ở left_bottom chứa sp của phiếu, tableSanPham chứa sp đang bán
     JScrollPane scrollTablePhieuNhap, scrollTableSanPham;
-    JPanel contentCenter, left_top, left_bottom, content_btn;
+    JPanel contentCenter, left_top, content_btn, left_bottom;
     DefaultTableModel tblModel, tblModelSP; //table co san 
-    ButtonCustom btnAddSp, btnEditSP, btnDelete, btnImport, btnNhapHang;
+    ButtonCustom btnAddSp, btnEditSP, btnDelete, btnNhapHang; //, btnImport
     InputForm txtMaphieu, txtNhanVien, txtMaSp, txtTenSp, txtDongia, txtMaISBN, txtSoLuongSPnhap ;
     SelectForm cbxNhaCungCap; //, cbxDanhMuc
     JTextField txtTimKiem;
@@ -147,23 +148,230 @@ public final class TaoPhieuNhap extends JPanel implements ItemListener, ActionLi
             }
         });
 
+
+        left = new PanelBorderRadius();
+        left.setLayout(new BorderLayout(0, 5));
+        left.setBackground(Color.white);
+
+        //content_CENTER (content_top + content_bottom) }-> left_top
+        contentCenter = new JPanel();
+        contentCenter.setPreferredSize(new Dimension(1200, 600));
+        contentCenter.setBackground(BackgroundColor);
+        contentCenter.setLayout(new BorderLayout(5, 5));
+        this.add(contentCenter, BorderLayout.CENTER);
+
+        left_top = new JPanel(); // Chứa tất cả phần ở phía trái trên cùng
+        left_top.setLayout(new BorderLayout());
+        left_top.setBorder(new EmptyBorder(5, 5, 10, 10));
+        left_top.setOpaque(false);
+
+        JPanel content_top, content_left, content_right, content_right_top;
+        content_top = new JPanel(new GridLayout(1, 2, 5, 5));
+        content_top.setOpaque(false);
+
+        content_left = new JPanel(new BorderLayout(5, 5));
+        content_left.setOpaque(false);
+        content_left.setPreferredSize(new Dimension(0, 300));
+
+        txtTimKiem = new JTextField();
+        txtTimKiem.setPreferredSize(new Dimension(100, 40));
+        txtTimKiem.putClientProperty("JTextField.placeholderText", "Tên sản phẩm, mã sản phẩm ...");
+        txtTimKiem.putClientProperty("JTextField.showClearButton", true);
+        txtTimKiem.putClientProperty("JTextField.leadingIcon", new FlatSVGIcon("./icon/search.svg"));
+
+        txtTimKiem.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent event) { //chạy khi nhả phím trong tìm kiếm
+                ArrayList<SanPhamDTO> rs = spBUS.search(txtTimKiem.getText(), "Tất cả");
+                loadDataTalbeSanPham(rs);
+            }
+        });
+
+        content_left.add(txtTimKiem, BorderLayout.NORTH);
+        content_left.add(scrollTableSanPham, BorderLayout.CENTER);
+
+        content_right = new JPanel(new BorderLayout(5, 5));
+        content_right.setOpaque(false);
+
+        content_right_top = new JPanel(new BorderLayout());
+        content_right_top.setPreferredSize(new Dimension(100, 250));
+        txtTenSp = new InputForm("Tên sản phẩm");
+        txtTenSp.setEditable(false);
+        txtTenSp.setPreferredSize(new Dimension(100, 90));
+        txtMaSp = new InputForm("Mã sản phẩm");
+        txtMaSp.setEditable(false);
+        txtMaISBN = new InputForm("Mã ISBN");
+        txtMaISBN.getTxtForm().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                ArrayList<SanPhamDTO> rs = spBUS.search(txtMaISBN.getText(), "ISBN");
+                loadDataTalbeSanPham(rs);
+            //thêm load lại inputform
+            }
+        });
+        txtDongia = new InputForm("Giá nhập");
+        PlainDocument dongia = (PlainDocument) txtDongia.getTxtForm().getDocument();
+        dongia.setDocumentFilter((new NumericDocumentFilter()));   //chỉ cho nhập số
+        txtSoLuongSPnhap = new InputForm("Số lượng");
+        PlainDocument soluong = (PlainDocument) txtSoLuongSPnhap.getTxtForm().getDocument();
+        soluong.setDocumentFilter((new NumericDocumentFilter())); //chỉ cho nhập số
+
+        JPanel merge1 = new JPanel(new BorderLayout());
+        merge1.setPreferredSize(new Dimension(100, 50));
+        merge1.add(txtMaSp, BorderLayout.WEST);
+        merge1.add(txtMaISBN, BorderLayout.CENTER);
+
+        JPanel merge2 = new JPanel(new GridLayout());
+        merge2.setPreferredSize(new Dimension(100, 80));
+        merge2.add(txtDongia);
+        merge2.add(txtSoLuongSPnhap);
+
+        content_right_top.add(txtTenSp, BorderLayout.NORTH);
+        content_right_top.add(merge1, BorderLayout.CENTER);
+        content_right_top.add(merge2, BorderLayout.SOUTH);
+        content_right.add(content_right_top, BorderLayout.NORTH);
+
+        content_top.add(content_left);
+        content_top.add(content_right);
+        left_top.add(content_top, BorderLayout.CENTER);
+
+        //content_btn  -  4 nút ở left_top (South) 
+        content_btn = new JPanel();
+        content_btn.setPreferredSize(new Dimension(0, 47));
+        content_btn.setLayout(new GridLayout(1, 4, 5, 5));
+        content_btn.setBorder(new EmptyBorder(8, 5, 0, 10));
+        content_btn.setOpaque(false);
+        btnAddSp = new ButtonCustom("Thêm sản phẩm", "success", 14);
+        btnEditSP = new ButtonCustom("Sửa sản phẩm", "warning", 14);
+        btnDelete = new ButtonCustom("Xoá sản phẩm", "danger", 14);
+            // btnImport = new ButtonCustom("Nhập Excel", "excel", 14);
+        btnAddSp.addActionListener(this);
+        btnEditSP.addActionListener(this);
+        btnDelete.addActionListener(this);
+            // btnImport.addActionListener(this);
+        btnEditSP.setEnabled(false);
+        btnDelete.setEnabled(false);
+        content_btn.add(btnAddSp);
+            // content_btn.add(btnImport);
+        content_btn.add(btnEditSP);
+        content_btn.add(btnDelete);
+        left_top.add(content_btn, BorderLayout.SOUTH);
+
+        //left_bottom này là danh sách nhập ở left phía nam
+        left_bottom = new JPanel();
+        left_bottom.setOpaque(false);
+        left_bottom.setPreferredSize(new Dimension(0, 250));
+        left_bottom.setBorder(new EmptyBorder(0, 5, 10, 10));
+        BoxLayout boxly = new BoxLayout(left_bottom, BoxLayout.Y_AXIS);
+        left_bottom.setLayout(boxly);
+        left_bottom.add(scrollTablePhieuNhap);
+        left.add(left_top, BorderLayout.CENTER);
+        left.add(left_bottom, BorderLayout.SOUTH);
+
+        // RIGHT 
+        right = new PanelBorderRadius();
+        right.setPreferredSize(new Dimension(320, 0));
+        right.setBorder(new EmptyBorder(5, 5, 5, 5));
+        right.setLayout(new BorderLayout());
+
+        JPanel right_top, right_center, right_bottom, pn_tongtien;
+        right_top = new JPanel(new GridLayout(4, 1, 0, 0));
+        right_top.setPreferredSize(new Dimension(300, 360));
+        right_top.setOpaque(false);
+        txtMaphieu = new InputForm("Mã phiếu nhập");
+        txtMaphieu.setText("PN" + maphieunhap);
+        txtMaphieu.setEditable(false);
+        txtNhanVien = new InputForm("Nhân viên nhập");
+        txtNhanVien.setText(nvDto.getHOTEN());
+        txtNhanVien.setEditable(false);
+        cbxNhaCungCap = new SelectForm("Nhà cung cấp", nccBus.getArrTenNhaCungCap());
+        right_top.add(txtMaphieu);
+        right_top.add(txtNhanVien);
+        right_top.add(cbxNhaCungCap);
+
+        right_center = new JPanel();
+        right_center.setPreferredSize(new Dimension(100, 100));
+        right_center.setOpaque(false);
+
+        right_bottom = new JPanel(new GridLayout(2, 1));
+        right_bottom.setPreferredSize(new Dimension(300, 100));
+        right_bottom.setBorder(new EmptyBorder(10, 10, 10, 10));
+        right_bottom.setOpaque(false);
+
+        pn_tongtien = new JPanel(new FlowLayout(1, 20, 0));
+        pn_tongtien.setOpaque(false);
+        JLabel lbltien = new JLabel("TỔNG TIỀN: ");
+        lbltien.setFont(new Font(FlatRobotoFont.FAMILY, 1, 18));
+        lbltongtien = new JLabel("0đ");
+        lbltongtien.setFont(new Font(FlatRobotoFont.FAMILY, 1, 18));
+        lbltien.setForeground(new Color(255, 51, 51));
+        pn_tongtien.add(lbltien);
+        pn_tongtien.add(lbltongtien);
+        right_bottom.add(pn_tongtien);
+
+        btnNhapHang = new ButtonCustom("Nhập hàng", "excel", 14);
+        btnNhapHang.addActionListener(this);
+        right_bottom.add(btnNhapHang);
+
+        right.add(right_top, BorderLayout.NORTH);
+        right.add(right_center, BorderLayout.CENTER);// để trống
+        right.add(right_bottom, BorderLayout.SOUTH);
+
+        contentCenter.add(left, BorderLayout.CENTER);
+        contentCenter.add(right, BorderLayout.EAST);
     }
 
-    public void actionbtn(String type) {
+    public void actionbtn(String type) { //ẩn hiện button
         boolean val_1 = type.equals("add");
         boolean val_2 = type.equals("update");
         btnAddSp.setEnabled(val_1);
-        btnImport.setEnabled(val_1);
+        // btnImport.setEnabled(val_1);
         btnEditSP.setEnabled(val_2);
         btnDelete.setEnabled(val_2);
         content_btn.revalidate();
         content_btn.repaint();
     }
 
-    public void setFormChiTietPhieu(ChiTietPhieuNhapDTO phieu) {
-        
+    public void setInfoSanPham(SanPhamDTO sp) { //set info vào inputform khi nhan ben tablesanpham
+        this.txtMaSp.setText(Integer.toString(sp.getMSP()));
+        this.txtTenSp.setText(sp.getTEN());
+        // ArrayList<SanPhamDTO> spdto = spBUS.getAll();;
+        // cbxDanhMuc.setArr(getThongTinSach(sp.getMSP()));
+        this.txtMaISBN.setText(sp.getISBN());
+        this.txtDongia.setText(Integer.toString(sp.getTIENN()));
     }
 
+    public void setFormChiTietPhieu(ChiTietPhieuNhapDTO phieu) { //set info vào inputform khi nhan ben tablephieunhap
+        SanPhamDTO ctsp = spBUS.getByMaSP(phieu.getMSP());
+
+        this.txtMaSp.setText(Integer.toString(ctsp.getMSP()));
+        this.txtTenSp.setText(spBUS.getByMaSP(ctsp.getMSP()).getTEN());
+        // this.cbxDanhMuc.setArr(getThongTinSach(pb.getDANHMUC()));
+        // this.cbxDanhMuc.setSelectedIndex(spBUS.getIndexByMaPhienBan(spdto, phieu.getMaphienbansp()));
+        this.txtDongia.setText(Integer.toString(phieu.getTIEN()));
+    }
+    
+    public void loadDataTalbeSanPham(ArrayList<SanPhamDTO> result) {
+        tblModelSP.setRowCount(0);
+        for (SanPhamDTO sp : result) {
+            tblModelSP.addRow(new Object[]{sp.getMSP(), sp.getTEN(), sp.getSL()});
+        }
+    }
+
+    public void resetForm() {
+        this.txtMaSp.setText("");
+        this.txtTenSp.setText("");
+        // String[] arr = {"Chọn sản phẩm"};
+        // this.cbxDanhMuc.setArr(arr);
+        this.txtDongia.setText("");
+        this.txtSoLuongSPnhap.setText("");
+        this.txtMaISBN.setText("");
+    }
+
+    public ChiTietPhieuNhapDTO checkTonTai() {
+        ChiTietPhieuNhapDTO p = phieunhapBus.findCT(chitietphieu, Integer.parseInt(txtMaSp.getText()) );
+        return p;
+    }
 
 
 
