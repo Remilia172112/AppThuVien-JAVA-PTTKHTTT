c1c3cfc5c05360b23a55cef362bc4fd1042a7867
MinhMafia <letheminha4.2022@gmail.com>
2024-05-04 00:34:33 +0700

    trang_chu

--- src/GUI/Panel/TrangChu.java
+++ src/GUI/Panel/TrangChu.java
@@ -2,6 +2,8 @@ package GUI.Panel;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import GUI.Component.PanelShadow;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 
@@ -12,9 +14,9 @@ public class TrangChu extends JPanel {
     JPanel info[];
     JLabel title, subTit, infoDetail[], objDetail[], objDetail1[], infoIcon[];
         String[][] getSt = {
-        {"Tính tiện lợi", "tinhchinhxac_128px.svg", "<html><br>Có tính năng tìm kiếm nhanh chóng <br><br>giúp người dùng dễ dàng tìm sách<br><br> theo tiêu chí cụ thể như tiêu đề, tác <br><br>giả hoặc thể loại.</html>"},
-        {"Tính bảo mật", "tinhbaomat_128px.svg", "<html><br>Thông tin cá nhân và thông tin liên <br><br>quan đến sách mượn thường được <br><br>bảo mật và chỉ được truy cập bởi <br><br>người dùng hoặc những người được <br><br>ủy quyền.</html>"},
-        {"Tính hiệu quả", "tinhhieuqua_128px.svg", "<html><br>Dễ dàng xác định được thông tin <br><br>về từng cuốn sách một cách nhanh <br><br>chóng và chính xác, giúp cho việc <br><br>quản lý sách được thực hiện một <br><br>cách hiệu quả hơn.</html>"},
+        {"Tính <br><br>tiện <br><br>lợi", "convenient_100px.svg", "<html>Có tính năng tìm kiếm nhanh chóng giúp người dùng dễ dàng<br><br> tìm sách theo tiêu chí cụ thể như tiêu đề, tác giả thể loại hoặc <br><br>theo mã ISBN sẽ đảm bảo tính chính xác và độ tin cậy cao hơn .</html>"},
+        {"Tính <br><br>bảo <br><br>mật", "secure_100px.svg", "<html>Thông tin cá nhân và thông tin liên quan đến sách mượn thường <br><br>được bảo mật và chỉ được truy cập bởi người dùng hoặc những <br><br>người được ủy quyền.</html>"},
+        {"Tính <br><br>hiệu <br><br>quả", "effective_100px.svg", "<html>Nhờ vào tính năng đặc biệt của mã ISBN giúp xác định được thông <br><br>tin về từng cuốn sách một cách nhanh chóng và chính xác, giúp <br><br>cho việc quản lý sách được thực hiện một cách hiệu quả hơn.</html>"},
     };
     Color MainColor = new Color(193,237,220);
     Color FontColor = new Color(96, 125, 139);
@@ -42,7 +44,10 @@ public class TrangChu extends JPanel {
         center = new JPanel();
         center.setBackground(BackgroundColor);
         center.setPreferredSize(new Dimension(1100, 800));
-        center.setLayout(new FlowLayout(1, 50, 50));
+        center.setLayout(new GridLayout(3 , 1 ,0,20));
+        center.setBorder(new EmptyBorder(30,110,30,220));
+        // center.setLayout(new FlowLayout(1, 50, 50));
+
 
         content = new PanelShadow[getSt.length];
         info = new JPanel[3];
@@ -50,13 +55,17 @@ public class TrangChu extends JPanel {
         objDetail = new JLabel[3];
         objDetail1 = new JLabel[3];
 
-        infoIcon = new JLabel[3];
+        // infoIcon = new JLabel[3];
 
         for (int i = 0; i < getSt.length; i++) {
-              
-              content[i] = new PanelShadow(getSt[i][1], getSt[i][0], getSt[i][2]);
-              center.add(content[i]);
-//            content[i] = new PanelShadow();
+            //   infoDetail[i].setIcon(new javax.swing.ImageIcon(getClass().getResource(getSt[i][1])));
+            //   objDetail[i].setText(getSt[i][0]);
+            //   objDetail1[i].setText(getSt[i][2]);
+            //   info[i].setLayout(getLayout());
+            content[i] = new PanelShadow();
+            content[i] = new PanelShadow(getSt[i][1], getSt[i][0], getSt[i][2]);
+
+            center.add(content[i]);
 //            content[i].setPreferredSize(new Dimension(300, 450));
 //            content[i].setBackground(MainColor);
 //            content[i].setLayout(new FlowLayout(1, 0, 10));
@@ -88,7 +97,7 @@ public class TrangChu extends JPanel {
 //            objDetail[i].setFont(new Font("Segoe UI", Font.PLAIN, 15));
 //            content[i].add(objDetail[i]);
 //
-//            center.add(content[i]);
+        //    center.add(content[i]);
 
         }
 
